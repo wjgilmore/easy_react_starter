07475131d3d015f054d7e9a417a4ba83eb7fef33
Jason Gilmore <wj@wjgilmore.com>
2015-06-11 12:55:27 -0400

    JSON example completed

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,52 @@
-var Hello = React.createClass({
-    render: function() {
-        return (
-         <h1>Welcome to VocabGiant</h1>
-        );
-    }
+var ListItem = React.createClass({
+  render: function() {
+    return (
+     <li>{this.props.item.origin}</li>
+    );
+  }
 });
 
-React.render(<Hello />, document.body);
+var ListItems = React.createClass({
+  render: function() {
+
+    var listItems = this.props.items.map(function(item){
+      return <ListItem key={item.origin} item={item} />;
+    });
+
+    return (
+     <ul>
+     {listItems}
+     </ul>
+    );
+
+  }
+});
+
+var Home = React.createClass({
+  getInitialState: function(){
+    return {
+      vocabulary: [],
+    }
+  },
+
+  componentDidMount: function() {
+    $.get(this.props.url, function(result) {
+      var words = result.words;
+      if (this.isMounted()) {
+        this.setState({
+          vocabulary: words
+        });
+      }
+    }.bind(this));
+  },
+
+  render: function() {
+    return (
+      <div>
+        <ListItems items={this.state.vocabulary} />
+      </div>
+    );
+  }
+});
+
+React.render(<Home url="http://localhost:3000/vocabulary.json" />, document.body);
