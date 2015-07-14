182625e887d6b7c53da324e6e113498e30e2443e
Jason Gilmore <wj@wjgilmore.com>
2015-07-14 12:52:50 -0400

    Added branch demonstrating dynamically updating inline styles

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,55 @@
 "use strict";
 
 var Hello = React.createClass({
-    render: function() {
-        return (
-         <h1>Welcome to VocabGiant</h1>
-        );
-    }
+
+  handleChange: function() {
+    this.props.onUserInput();
+  },
+
+	render: function() {
+
+		var headerStyle = {
+			color: '#CF4A2B',
+			borderWidth: this.props.border,
+			borderStyle: 'solid'
+		};
+
+    return (
+     <div>
+       <h1 key="welcome" style={headerStyle}>Welcome to VocabGiant</h1>
+       <a href="#" ref="changeBorder" onClick={this.handleChange}>
+        Increase Border
+       </a>
+     </div>
+    );
+
+	}
+
 });
 
-React.render(<Hello />, document.getElementById('app'));
+var Home = React.createClass({
+
+  getInitialState: function(){
+    return {
+      borderWidth: 1
+    };
+  },
+
+  handleUserInput: function() {
+      this.setState({
+          borderWidth: this.state.borderWidth + 1
+      });
+  },
+
+  render: function() {
+    return (
+      <div>
+        <Hello 
+          border={this.state.borderWidth} 
+          onUserInput={this.handleUserInput} />
+      </div>
+    );
+  }
+});
+
+React.render(<Home />, document.getElementById('app'));
