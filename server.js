986a4ec922d72650ec6041da2cfebb9dbb908a99
Jason Gilmore <wj@wjgilmore.com>
2015-07-22 14:25:50 -0400

    Simplified server.js.

--- server.js
+++ server.js
@@ -10,20 +10,6 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req, res) {
-  fs.readFile('index.html', function(err, data) {
-    res.setHeader('Content-Type', 'text/html');
-    res.send(data);
-  });
-});
-
-app.get('/test', function(req, res) {
-  fs.readFile('test.html', function(err, data) {
-    res.setHeader('Content-Type', 'text/html');
-    res.send(data);
-  });
-});
-
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
